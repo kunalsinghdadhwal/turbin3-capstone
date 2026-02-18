ee0ab57c7d69a9fb73bf5fdf5cac87b4759b59b6
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2026-02-18 09:48:49 +0530

    deposit instructions
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

--- programs/multisig-vault/src/instructions/deposit_sol.rs
+++ programs/multisig-vault/src/instructions/deposit_sol.rs
@@ -1,1 +1,34 @@
+use crate::constants::*;
+use crate::state::VaultConfig;
+use anchor_lang::prelude::*;
+use anchor_lang::system_program;
 
+#[derive(Accounts)]
+pub struct DepositSol<'info> {
+    #[account(mut)]
+    pub depositor: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [VAULT_SEED, vault_config.creator.as_ref()],
+        bump = vault_config.bump,
+    )]
+    pub vault_config: Account<'info, VaultConfig>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<DepositSol>, amount: u64) -> Result<()> {
+    system_program::transfer(
+        CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.depositor.to_account_info(),
+                to: ctx.accounts.vault_config.to_account_info(),
+            },
+        ),
+        amount,
+    )?;
+
+    Ok(())
+}
--- programs/multisig-vault/src/instructions/deposit_token.rs
+++ programs/multisig-vault/src/instructions/deposit_token.rs
@@ -1,1 +1,54 @@
+use crate::constants::*;
+use crate::state::VaultConfig;
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{transfer, Mint, Token, TokenAccount, Transfer};
 
+#[derive(Accounts)]
+pub struct DepositToken<'info> {
+    #[account(mut)]
+    pub depositor: Signer<'info>,
+
+    #[account(
+        seeds = [VAULT_SEED, vault_config.creator.as_ref()],
+        bump = vault_config.bump,
+    )]
+    pub vault_config: Account<'info, VaultConfig>,
+
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = depositor,
+    )]
+    pub depositor_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = depositor,
+        associated_token::mint = mint,
+        associated_token::authority = vault_config,
+    )]
+    pub vault_ata: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<DepositToken>, amount: u64) -> Result<()> {
+    transfer(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.depositor_ata.to_account_info(),
+                to: ctx.accounts.vault_ata.to_account_info(),
+                authority: ctx.accounts.depositor.to_account_info(),
+            },
+        ),
+        amount,
+    )?;
+
+    Ok(())
+}
